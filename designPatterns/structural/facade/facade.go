2cf2631cfc081a355c0d5f0313ee3792fa974bb5
Olumide Ogundele <olumideralph@gmail.com>
2019-09-27 06:11:38 +0100

    finish the implementation of the facade pattern

--- designPatterns/structural/facade/facade.go
+++ designPatterns/structural/facade/facade.go
@@ -81,3 +81,34 @@ func newBranchManager() *BranchManagerFacade {
 
 	return branchManagerFacade
 }
+
+// method for branch manager to create a customer account
+func(branchManager *BranchManagerFacade) createCustomerAccount(customerName, accountType string) (*Customer, *Account) {
+	customer := branchManager.customer.create(customerName)
+	account := branchManager.account.create(accountType)
+
+	return customer, account
+}
+
+// method for the new branch manager to perform a transaction
+func(branchManager *BranchManagerFacade) createTransaction(srcAccountID, destAccountID string, amount float32) *Transaction {
+	transaction := branchManager.transaction.create(srcAccountID, destAccountID, amount)
+
+	return transaction
+}
+
+func main()  {
+	// create the new branch manager facade
+	branchManager := newBranchManager()
+
+	// create a customer account
+	customer, account := branchManager.createCustomerAccount("Olumide Ogundele", "Current Account")
+
+	fmt.Printf("Customer name: %s \n", customer.name)
+	fmt.Printf("Customer's account type: %s \n", account.accountType)
+
+	// perform a transaction
+	transaction := branchManager.createTransaction("45678", "98576", 200000.56)
+
+	fmt.Printf("Current transaction amount: %g \n", transaction.amount)
+}
