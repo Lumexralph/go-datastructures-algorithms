eb450e2f5e9d1c96aa478e5cbe902d9ed05684bd
Olumide Ogundele <olumideralph@gmail.com>
2019-10-17 16:23:27 +0100

    implement linkedlist with methods to modify the list

--- dataStructures/linkedList/linkedList.go
+++ dataStructures/linkedList/linkedList.go
@@ -25,6 +25,45 @@ func(linkedList *LinkedList) AddToHead(property int) {
 	linkedList.headNode = &node
 }
 
+// IterateList method iterates over a LinkedList
+func (linkedList *LinkedList) IterateList() {
+	node := linkedList.headNode
+	for node != nil {
+		fmt.Printf("%d ", node.property)
+		node = node.nextNode
+	}
+	fmt.Println("")
+}
+
+// GetLastNode method returns the last node in the linkedlist
+func (linkedList *LinkedList) GetLastNode() *Node {
+	var lastNode *Node
+
+	node := linkedList.headNode
+
+	for node != nil {
+		lastNode = node
+		node = node.nextNode
+	}
+
+	return lastNode
+}
+
+// AddToEnd method adds the node at the end of the list
+func(linkedList *LinkedList) AddToEnd(property int) {
+	// create new node as the last node
+	newNode := &Node{
+		property: property,
+		nextNode: nil,
+	}
+
+	// get the last node
+	lastNode := linkedList.GetLastNode()
+	if lastNode != nil {
+		lastNode.nextNode = newNode
+	}
+}
+
 func main() {
 	linkedList := LinkedList{}
 
@@ -33,4 +72,7 @@ func main() {
 
 	linkedList.AddToHead(3)
 	fmt.Printf("The head node has a property %d\n", linkedList.headNode.property)
+
+	linkedList.AddToEnd(5)
+	linkedList.IterateList()
 }
