8b47d24d5b85c4e26178873b1d95481dc2bfba74
Olumide Ogundele <olumideralph@gmail.com>
2019-10-21 16:24:25 +0100

    implement add to head method

--- doublyLinkedList/doublyLinkedList.go
+++ doublyLinkedList/doublyLinkedList.go
@@ -33,8 +33,28 @@ func (doublyLinkedList *DoublyLinkedList) NodeBetweenValues(firstProperty string
 	return nil
 }
 
+// AddToHead method of the doubly LinkedList sets the previousNode
+// property of the current headNode of the linked list to the node
+// that's added with property
+func (doublyLinkedList *DoublyLinkedList) AddToHead(property string) {
+	newNode := &Node{
+		property: property,
+		prevNode: nil,
+		nextNode: nil,
+	}
+	headNode := doublyLinkedList.headNode
+
+	if headNode != nil {
+		newNode.nextNode = headNode
+		headNode.prevNode = newNode
+	}
+
+	doublyLinkedList.headNode = newNode
+}
+
 func main() {
 	doublyLinkedList := DoublyLinkedList{}
+	doublyLinkedList.AddToHead("c")
 
 	fmt.Printf("The node between properties %v", doublyLinkedList.NodeBetweenValues("a", "b"))
 }
